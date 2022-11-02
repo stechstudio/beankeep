060f7dfd1b112fe1961c7526a8a087e1a1e73a00
Zeb DeOs <zebulin@stechstudio.com>
2022-11-02 15:11:13 -0400

    Beankeep: smoke test

---
+++ src/Beankeep.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace STS\Beankeep;
+
+class Beankeep
+{
+    public static function truth(): bool
+    {
+        return true;
+    }
+
+    public static function error(): bool
+    {
+        return false;
+    }
+}
