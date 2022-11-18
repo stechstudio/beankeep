7d90a2e843bc159018dad6cc583812530db055e1
Zeb DeOs <zebulin@stechstudio.com>
2022-11-18 13:38:22 -0500

    SourceDocument: restructure test class a bit

--- tests/Values/SourceDocumentTest.php
+++ tests/Values/SourceDocumentTest.php
@@ -11,24 +11,29 @@ use DateTimeImmutable;
 
 final class SourceDocumentTest extends TestCase
 {
+    protected $dateStr = '2021-09-09 05:00:00 -08:00';
+
+    protected $memo = 'Reticulate the splines';
+
+    protected $attachment = '2021-09-09-splines-r-us--reticulating-service-invoice.pdf';
+
+    protected function date(?string $dateStr=null): DateTimeImmutable
+    {
+        return new \DateTimeImmutable($dateStr ?? $this->dateStr);
+    }
+
     public function testMemberValues(): void
     {
         $sourceDocument = new SourceDocument(
             id: 1,
-            date: new \DateTimeImmutable('2021-09-09 05:00:00 -08:00'),
-            memo: 'Reticulate the splines',
-            attachment: '2021-09-09-splines-r-us--reticulating-service-invoice.pdf',
+            date: $this->date(),
+            memo: $this->memo,
+            attachment: $this->attachment,
         );
 
         $this->assertEquals(1, $sourceDocument->id);
-        $this->assertEquals(
-            new \DateTimeImmutable('2021-09-09 05:00:00 -08:00'),
-            $sourceDocument->date,
-        );
-        $this->assertEquals('Reticulate the splines', $sourceDocument->memo);
-        $this->assertEquals(
-            '2021-09-09-splines-r-us--reticulating-service-invoice.pdf',
-            $sourceDocument->attachment,
-        );
+        $this->assertEquals($this->date(), $sourceDocument->date);
+        $this->assertEquals($this->memo, $sourceDocument->memo);
+        $this->assertEquals($this->attachment, $sourceDocument->attachment);
     }
 }
