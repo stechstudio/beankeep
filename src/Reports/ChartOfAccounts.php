bfecaafeb185a1b3e393777f61a028dc6b8bbbc3
Zeb DeOs <zebulin@stechstudio.com>
2022-11-21 16:24:01 -0500

    ChartOfAccounts: play with container for accounts

---
+++ src/Reports/ChartOfAccounts.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace STS\Beankeep\Reports;
+
+use STS\Beankeep\Enums\AccountType;
+use STS\Beankeep\Interfaces\AccountInterface as Account;
+
+final readonly class ChartOfAccounts
+{
+    /**
+     * @param array<Account> $accounts
+     */
+    public function __construct(public array $accounts)
+    {
+    }
+
+    /**
+     * @param array<Account> $accounts
+     */
+    public static function make(array $accounts): self {
+        return new self($accounts);
+    }
+}
